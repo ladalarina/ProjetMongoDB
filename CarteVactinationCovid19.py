d10164321a920d37b171210b43266ccd3bbafd9a
lada <larinalada@gmail.com>
2022-04-20 22:56:46 +0200

    bonus et test

--- CarteVactinationCovid19.py
+++ CarteVactinationCovid19.py
@@ -6,26 +6,28 @@ import folium
 from branca.element import Template, MacroElement
 from math import *
 
-### connection a au serveur et à la bd
+from sympy import li
+
+# connection a au serveur et à la bd
 db_uri = "mongodb+srv://etudiant:ur2@clusterm1.0rm7t.mongodb.net/"
 client = MongoClient(db_uri)
 db = client["doctolib"]
 coll = db["dump_Jan2022"]
 # print(coll.index_information())
 
-##definition des variables
+# definition des variables
 lat = 48.117266
 long = -1.6777926
 chezMoi = {"type": "Point", "coordinates": [long, lat]}
 date_deb = datetime.strptime("2022-01-26", "%Y-%m-%d")
 date_fin = datetime.strptime("2022-01-29", "%Y-%m-%d")
-#requete agregate
+# requete agregate
 cursor = coll.aggregate(
     [
         {
             "$geoNear": {
                 "distanceField": "distance",
-                "maxDistance":50000,
+                "maxDistance": 50000,
                 "near": chezMoi
             }
         },
@@ -48,12 +50,12 @@ cursor = coll.aggregate(
                 },
                 "nb": {"$sum": 1}
             }
-         }
+        }
     ]
 )
-# print(len(list(cursor))) 
-##--> 7
-#creation de la carte
+# print(len(list(cursor)))
+# --> 7
+# creation de la carte
 
 m = folium.Map(location=[lat, long], zoom_start=9,
                tiles='Stamen Toner', control_scale=True)
@@ -68,15 +70,15 @@ tertile2 = tertile * 2
 for i in res:
     coord = i["_id"]["coord"]
     if i["nb"] <= tertile:
-        color='red'
+        color = 'red'
     elif i["nb"] <= 2*tertile:
-        color='orange'
+        color = 'orange'
     else:
-        color='green'
-    folium.Marker([coord[1], coord[0]], popup = i["_id"]["nom"]+ "\n Nombre de places: "+str(i["nb"]),
-                 icon = folium.Icon(color=color, icon='medkit', prefix='fa')).add_to(m)
-outfp="docs/base_map.html"
-#légende
+        color = 'green'
+    folium.Marker([coord[1], coord[0]], popup=i["_id"]["nom"] + "\n Nombre de places: "+str(i["nb"]),
+                  icon=folium.Icon(color=color, icon='medkit', prefix='fa')).add_to(m)
+outfp = "docs/base_map.html"
+# légende
 template = """
 {% macro html(this, kwargs) %}
 <div
@@ -93,7 +95,7 @@ template = """
         <div style="padding: 10px; color: white; width: 35px;height: 35px; background-color: orange;">
             <i class="fa-rotate-0 fa fa-medkit"></i>
         </div>
-        Entre """ + str(tertile) +""" et """ + str(tertile2) + """</li>
+        Entre """ + str(tertile) + """ et """ + str(tertile2) + """</li>
     <li>
         <div style="padding: 10px; color: white; width: 35px;height: 35px; background-color: red;">
             <i class="fa-rotate-0 fa fa-medkit"></i>
@@ -110,3 +112,90 @@ m.get_root().add_child(macro)
 m.save(outfp)
 
 
+# Visualisation 2
+
+date_deb1 = datetime.strptime("2022-01-01", "%Y-%m-%d")
+date_fin1 = datetime.strptime("2022-06-01", "%Y-%m-%d")
+# requete agregate
+# db.users.findOne({"username" : {$regex : "son"}});
+cursor1 = coll.aggregate(
+    [
+
+        {"$unwind": "$visit_motives"},
+        {"$unwind": "$visit_motives.name"},
+        {"$unwind": "$visit_motives.slots"},
+        {"$match":
+            {
+                "visit_motives.slots": {
+                    "$gte": date_deb1,
+                    "$lte": date_fin1
+                },
+                "visit_motives.name": {
+                    "$regex": "1re injection"
+                }
+            }
+         },
+        {
+            "$group": {
+                "_id": {
+                    "nom": "$name",
+                    "coord": "$location.coordinates",
+                    "url": "$url_doctolib",
+                    "visit_motives" :  "$visit_motives.name" ,
+                },
+                
+                "nb": {"$sum": 1}
+                
+            }
+        }
+    ]
+)
+#print(len(list(cursor1)))
+
+## carte 2
+m1 = folium.Map(location=[lat, long], zoom_start=9,
+               tiles='Stamen Toner', control_scale=True)
+
+res1 = list(cursor1)
+
+libelles = []
+for i in res1:
+    libelles.append(i["_id"]["visit_motives"])
+unique_libelle = list(set(libelles))
+colors = [
+    "green",
+    "blue",
+    "purple",
+    "yellow",
+    "red",
+    "pink"
+]
+for i in res1:
+    coord = i["_id"]["coord"]
+    injection = i["_id"]["visit_motives"]
+    print(injection)
+    color = colors[unique_libelle.index(injection) % (len(color) - 1)]
+    folium.Marker([coord[1], coord[0]], popup=i["_id"]["nom"] + i["_id"]["visit_motives"],
+        icon=folium.Icon(color=color, icon='medkit', prefix='fa')).add_to(m1)
+
+lis = []
+for lib in unique_libelle:
+    lis.append("<li><div style=\"width: 25px; height: 25px; background-color: " + colors[unique_libelle.index(lib) % (len(color) - 1)] + " \"></div>" + lib + "</li>")
+template1 = """
+{% macro html(this, kwargs) %}
+<div id="lada"
+    style='position: absolute; z-index:9999; border:2px solid grey; background-color:rgba(255, 255, 255, 0.8);
+     border-radius:6px; padding: 10px; font-size:14px; right: 20px; bottom: 20px;'>
+    <div class='legend-scale'>
+    <ul class='legend-labels'>
+        """ + "".join(lis) + """
+    </ul>
+    </div>
+</div>
+{% endmacro %}"""
+
+outfp = "docs/base_map1.html"
+macro = MacroElement()
+macro._template = Template(template1)
+m1.get_root().add_child(macro)
+m1.save(outfp)
--- docs/exo1.md
+++ docs/exo1.md
@@ -3,8 +3,15 @@
 Cette carte représente les centres de vaccination, ouverts entre le 26 et 29 janvier 2022 inclus,
 dans un rayon maximal de 50 km de Rennes. Les centres qu'on peut apercevoir sur cette carte se distinguent en fonction de nombre de créneaux disponibles: 
 - rouge: pour un nombre de créneaux faible
-- orange - pour un nombre de créneaux moyens
+- orange: pour un nombre de créneaux moyens
 - vert: pour beaucoup de créneaux disponibles.
+
+
+
 ### Carte des centres de vaccination ouverts entre le 26 et 29 janvier 2022
 
+
 <iframe src="base_map.html" class="is-fullwidth" height="500px" width="100%"></iframe>
+<iframe src="base_map1.html" class="is-fullwidth" height="500px" width="100%"></iframe>
+
+<script>console.log("test")</script>
